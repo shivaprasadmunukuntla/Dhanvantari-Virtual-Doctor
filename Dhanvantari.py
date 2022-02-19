b5659c4cdf1fc8f0a3b949cdc260d986dec6dd27
Shiva Prasad <50986624+shivaprasadmunukuntla@users.noreply.github.com>
2022-02-19 19:55:44 +0530

    Main/

---
+++ Dhanvantari.py
@@ -0,0 +1,116 @@
+import nltk
+import numpy
+import tflearn
+import random
+import json
+from tkinter import *
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1500, batch_size=8, show_metric=True)
+model.save("model.tflearn")
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+
+root = Tk()
+
+
+def send():
+    send = "You => " + e.get()
+    txt.insert(END, "\n" + send)
+    inp = e.get()
+    if inp.lower() == "quit":
+        exit()
+
+    results = model.predict([bag_of_words(inp, words)])
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+
+    for tg in data["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
+
+    e.get()
+    txt.insert(END, "\n" + "Dr.Dhanvantari :" + random.choice(responses))
+    e.delete(0, END)
+
+
+txt = Text(root)
+txt.grid(row=0, column=0, columnspan=2)
+e = Entry(root, width=100)
+
+send = Button(root, text="Send", bg="blue", fg="white", font=("time new roman", 11, "bold"), command=send).grid(row=1, column=1)
+e.grid(row=1, column=0)
+root.title("Start talking with Dr.Dhanvantari (quit to stop)!")
+root.configure(background="lightgreen")
+root.mainloop()
